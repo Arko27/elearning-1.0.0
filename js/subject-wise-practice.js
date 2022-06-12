dc2bc1f3c2618bfb4e2e261e9cda16ff00f9f5e6
bhaskar sengupta <sajal1943@gmail.com>
2022-06-12 19:20:25 +0530

    search bar inplemented

--- js/subject-wise-practice.js
+++ js/subject-wise-practice.js
@@ -1,9 +1,36 @@
-import api from './apis.js'
-import './main.js';
+import api from "./apis.js";
+import "./main.js";
 
-function addTopics(topicName,subject,topic)
-{
-    let box = `<div class="col-lg-3 col-md-6 wow fadeInRight" data-wow-delay="0.1s">
+let topics = [];
+document.getElementById("search-bar").addEventListener("change", (e) => {
+  e.preventDefault();
+  if (e.target.value != "") {
+    const params = new URLSearchParams(document.location.search);
+    const Subject = params.get("subject");
+
+    console.log(topics);
+    var NodesString = "";
+    console.log(e.target.value);
+    var data = topics.filter((elem, index) =>
+      elem.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    console.log(data);
+    data.forEach((elem, i) => {
+      console.log(elem.name);
+      NodesString += addTopics(
+        String(i + 1) + ". " + elem.name,
+        Subject,
+        elem.name
+      );
+    });
+    var UlElement = document.getElementById("topic-box");
+    UlElement.innerHTML = "";
+    UlElement.insertAdjacentHTML("beforeend", NodesString);
+  }
+});
+
+function addTopics(topicName, subject, topic) {
+  let box = `<div class="col-lg-3 col-md-6 wow fadeInRight" data-wow-delay="0.1s">
                     <div class="course-item bg-light">
                         <div class="position-relative overflow-hidden">
                             <img class="img-fluid" src="img/course-1.jpg"alt="">
@@ -19,47 +46,48 @@ function addTopics(topicName,subject,topic)
                         </div>
                         <div class="d-flex border-top"></div>
                     </div>
-                </div>`
-    
-    return box
+                </div>`;
+
+  return box;
 }
 
-function AddNewElementUsingString()
-{
-    var NodesString = "";
-    var i = 0;
-    const params = new URLSearchParams(document.location.search);
-    console.log(params)
-    const Subject = params.get("subject");
-    fetch(`${api.get.topic}${Subject}`,{
-        method: "GET",
-        headers: {
-            Accept: 'application/json',
-            'Accept-encoding': 'gzip, deflate',
-            'Content-Type': 'application/json',
-        },
-    })
-
+function AddNewElementUsingString() {
+  var NodesString = "";
+  var i = 0;
+  const params = new URLSearchParams(document.location.search);
+  console.log(params);
+  const Subject = params.get("subject");
+  fetch(`${api.get.topic}${Subject}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Accept-encoding": "gzip, deflate",
+      "Content-Type": "application/json",
+    },
+  })
     .then((res) => res.json())
     .then((data) => {
-        // console.log(data.result)
-        data.result.forEach(elem => {
-            i++
-            console.log(elem.name);
-            NodesString += addTopics(String(i) + '. ' + elem.name,Subject,elem.name);
-    });
-    var UlElement = document.getElementById('topic-box');
-    UlElement.insertAdjacentHTML('beforeend', NodesString);
+      // console.log(data.result)
+      topics = data.result;
+      data.result.forEach((elem) => {
+        i++;
+        console.log(elem.name);
+        NodesString += addTopics(
+          String(i) + ". " + elem.name,
+          Subject,
+          elem.name
+        );
+      });
+      var UlElement = document.getElementById("topic-box");
+      UlElement.insertAdjacentHTML("beforeend", NodesString);
     });
 }
 
-AddNewElementUsingString()
+AddNewElementUsingString();
 
-document.getElementById('easy').addEventListener("click",()=>{
-    function top(){
-        a = document.getElementById("topic").innerText
-        return a
-    }
-})
-
-
+document.getElementById("easy").addEventListener("click", () => {
+  function top() {
+    a = document.getElementById("topic").innerText;
+    return a;
+  }
+});
